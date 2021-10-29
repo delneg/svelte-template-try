2684cf41fe17b5a3d38d659baf620d26a0d7e695
Denis Bobrov <delneg@yandex.ru>
2021-10-30 02:14:08 +0300

    Initial try (unsuccessful)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import './hmr'
 import App from './App.svelte'
+import type { Remote } from "comlink";
+import type { Api } from "./worker";
+import { initBackend } from "absurd-sql/dist/indexeddb-main-thread";
+import { wrap } from "comlink";
+
+const worker = new Worker(new URL("./worker", import.meta.url),{type:"module"});
+initBackend(worker);
+const api: Remote<Api> = wrap(worker);
+
+async function init() {
+  document.body.innerHTML = "Loading...";
+  await api.setup();
+  document.body.innerHTML = "Worker Ready";
+
+  await api.createUser();
+  const users = await api.getUsers();
+  document.body.innerHTML = "Result:" + JSON.stringify(users);
+  console.log(users);
+}
+
+init().catch(console.error);
 
 const app = new App({
   target: document.body,
---
+++ src/worker.ts
@@ -0,0 +1,86 @@
+import { SQLiteFS } from 'absurd-sql'
+import IndexedDBBackend from 'absurd-sql/dist/indexeddb-backend'
+import { Column, Connection, createConnection, Entity, PrimaryGeneratedColumn, Repository } from 'typeorm'
+import { expose } from 'comlink'
+import type { InitSqlJsStatic } from 'sql.js'
+import initSqlJs from '@jlongster/sql.js'
+async function setupTypeormEnvWithSqljs(dbPath: string) {
+  const SQL = await (initSqlJs as InitSqlJsStatic)({
+    locateFile: (file: string) => file,
+  });
+  // @ts-ignore
+  const sqlFS = new SQLiteFS(SQL.FS, new IndexedDBBackend());
+  // @ts-ignore
+  SQL.register_for_idb(sqlFS);
+  // @ts-ignore
+  SQL.FS.mkdir("/sql");
+  // @ts-ignore
+  SQL.FS.mount(sqlFS, {}, "/sql");
+  class PatchedDatabase extends SQL.Database {
+    constructor() {
+      // @ts-ignore
+      super(dbPath, { filename: true });
+      // Set indexeddb page size
+      this.exec(`PRAGMA page_size=8192;PRAGMA journal_mode=MEMORY;`);
+    }
+  }
+  const localStorageMock = {
+    getItem() {
+      return undefined;
+    },
+    setItem() {
+      return undefined;
+    },
+  };
+  // setup global env
+  Object.assign(globalThis as any, {
+    SQL: {
+      ...SQL,
+      Database: PatchedDatabase,
+    },
+    localStorage: localStorageMock,
+    window: globalThis,
+  });
+}
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+}
+
+let conn: Connection;
+let userRepository: Repository<User>;
+async function setup() {
+  // with /sql/ namespace
+  const DBNAME = "/sql/db.sqlite";
+  await setupTypeormEnvWithSqljs(DBNAME);
+  conn = await createConnection({
+    type: "sqljs", // this connection search window.SQL on browser
+    location: DBNAME,
+    autoSave: false, // commit by absurd-sql
+    synchronize: true,
+    entities: [User],
+    logging: ["query", "schema"],
+  });
+  userRepository = conn.getRepository(User);
+}
+
+async function createUser() {
+  const user = new User();
+  user.id = Math.floor(Math.random() * 10000);
+  user.name = "dummy-user-" + Math.random().toString();
+  await userRepository.save(user);
+  return true;
+}
+
+async function getUsers() {
+  return await userRepository.find();
+}
+
+const api = { setup, createUser, getUsers };
+export type Api = typeof api;
+
+expose(api);
